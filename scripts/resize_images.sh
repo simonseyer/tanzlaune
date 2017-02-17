388107bf681a62d93b9df02f201ecd78a479e4d3
Simon Seyer <simon.seyer@gmail.com>
2017-02-17 13:35:44 +0200

    Fixed deploy paths

--- scripts/resize_images.sh
+++ scripts/resize_images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mogrify -verbose -resize x800 docs/header/*
-mogrify -verbose -resize x400 docs/img/logo.png
-mogrify -verbose -resize x140 docs/img/partner/*
+mogrify -verbose -resize x800 public/header/*
+mogrify -verbose -resize x400 public/img/logo.png
+mogrify -verbose -resize x140 public/img/partner/*
